0b7bad9e224b07412a86f85a5789d1bb5ff98767
jonesmbindyo <jonesmbindyo@gmail.com>
2026-02-04 16:26:01 +0300

    feature: Add Koin module for HomeViewModel

--- app/src/main/java/com/zonkesoft/fooball/di/AppModules.kt
+++ app/src/main/java/com/zonkesoft/fooball/di/AppModules.kt
@@ -1,6 +1,23 @@
 package com.zonkesoft.fooball.di
 
+import com.zonkesoft.fooball.ui.viewmodel.HomeViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+/**
+ * Aggregated Koin modules in our app.
+ */
+val appModule = module {
+    // ViewModels
+    viewModel { HomeViewModel() }
+}
+
+
+/**
+ * All Koin modules in our app.
+ */
 val appModules = listOf(
+    appModule,
     coreModule,
     repositoryModule,
     /*networkModule,
