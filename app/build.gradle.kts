a7a6924e3e091578c89b36a5bd40497e62fe6474
jonesmbindyo <jonesmbindyo@gmail.com>
2026-02-05 13:08:28 +0300

    prod

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 23
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0.1-dev"
+        versionName = "1.0.1-prod"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
